6630214f334802864548a9e60576ee2317c9ba65
Fabricio Silva Carvalhal <fabcarvalhal@gmail.com>
2015-07-07 16:12:51 -0300

    listagem interna de receitas

---
+++ public_html/app/receitas.php
@@ -0,0 +1,88 @@
+<!-- Inicio visao geral -->
+
+<article class="ui 	sixteen wide centered column box">
+
+<h3 class="ui top attached header topat">
+
+<i class="list icon"></i>
+	<div class="content">
+	Receitas
+
+  </div>
+<a href="#" title="Adicionar Receita" class="addicon">
+
+  <i class="plus icon inverted green icone"></i> 
+
+</a>
+</h3>
+
+
+<div class="ui attached segment">
+
+
+	
+
+<div class="ui selection relaxed list listViGeral">
+  <a class="item">
+   <div class="ui right floated green label">R$ 100</div>
+    <i class="minus icon green"></i>
+    <div class="content">
+      <div class="header">Receita 1
+  
+        
+    </div>
+      
+    </div>
+  </a>
+
+  <a class="item">
+   <div class="ui right floated green label">R$ 100</div>
+    <i class="minus icon green"></i>
+    <div class="content">
+      <div class="header">Receita 2
+  
+        
+    </div>
+      
+    </div>
+  </a>
+
+  <a class="item">
+   <div class="ui right floated green label">R$ 100</div>
+    <i class="minus icon green"></i>
+    <div class="content">
+      <div class="header">Receita 3
+  
+        
+    </div>
+      
+    </div>
+  </a>
+
+
+  <a class="item">
+   <div class="ui right floated green label">R$ 100</div>
+    <i class="minus icon green"></i>
+    <div class="content">
+      <div class="header">Receita 4
+  
+        
+    </div>
+      
+    </div>
+  </a>
+
+  
+ 
+	</div>
+</div>
+
+
+
+</article>
+
+
+<!-- Fim visao geral -->
+
+
+
