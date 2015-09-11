798d72212ffcd9836e5528925f3db6d398e407eb
Fabricio Silva Carvalhal <fabcarvalhal@gmail.com>
2015-09-11 17:32:51 -0300

    novas paginas

---
+++ public_html/app/contascard.php
@@ -0,0 +1,206 @@
+<!--Inicio card contas corrente -->
+		<div class="row">
+			<div class="col-md-10 col-lg-8 col-lg-offset-2 col-md-offset-1">
+				<div class="box">
+					<div class="box-icon">
+						<span class="fa fa-3x fa-briefcase"></span>
+
+					</div>
+					<div class="info">
+						<h4 class="text-center">Contas Corrente</h4>
+
+						
+						<ul class="listviGeral">
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 1
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 2
+											</span>
+											<span class="valorItem backred pull-right"> 
+												R$ -200,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 3
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+						</ul>
+
+						
+					</div>
+
+				</div>
+
+			</div>
+
+		</div><!-- Fim card contas correntes-->
+
+<!--Inicio card contas Poupança -->
+		<div class="row">
+			<div class="col-md-10 col-lg-8 col-lg-offset-2 col-md-offset-1">
+				<div class="box">
+					<div class="box-icon">
+						<span class="fa fa-3x fa-money"></span>
+						
+					</div>
+					<div class="info">
+						<h4 class="text-center">Contas Poupança</h4>
+
+						
+						<ul class="listviGeral">
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 1
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 2
+											</span>
+											<span class="valorItem backred pull-right"> 
+												R$ -200,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 3
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+						</ul>
+
+						
+					</div>
+
+				</div>
+
+			</div>
+
+		</div><!-- Fim card contas Poupança-->
+
+<!--Inicio card contas AOC -->
+		<div class="row">
+			<div class="col-md-10 col-lg-8 col-lg-offset-2 col-md-offset-1">
+				<div class="box">
+					<div class="box-icon">
+						<span class="fa fa-3x fa-bank"></span>
+
+					</div>
+					<div class="info">
+						<h4 class="text-center">Contas Alguma Outra Coisa</h4>
+
+						
+						<ul class="listviGeral">
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 1
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 2
+											</span>
+											<span class="valorItem backred pull-right"> 
+												R$ -200,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 3
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+						</ul>
+
+						
+					</div>
+
+				</div>
+
+			</div>
+
+		</div><!-- Fim card contas AOC-->
---
+++ public_html/app/despesascard.php
@@ -0,0 +1,208 @@
+<!--Inicio card contas corrente -->
+		<div class="row">
+			<div class="col-md-10 col-lg-8 col-lg-offset-2 col-md-offset-1">
+				<div class="box">
+					<div class="box-icon">
+						<span class="fa fa-3x fa-briefcase"></span>
+
+					</div>
+					<div class="info">
+						<h4 class="text-center">{Categoria}</h4>
+
+						
+						<ul class="listviGeral">
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 1
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 2
+											</span>
+											<span class="valorItem backred pull-right"> 
+												R$ -200,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 3
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+						</ul>
+
+						
+					</div>
+
+				</div>
+
+			</div>
+
+		</div><!-- Fim card contas correntes-->
+
+<!--Inicio card contas Poupança -->
+		<div class="row">
+			<div class="col-md-10 col-lg-8 col-lg-offset-2 col-md-offset-1">
+				<div class="box">
+					<div class="box-icon">
+						<span class="fa fa-3x fa-money"></span>
+
+					</div>
+					<div class="info">
+						<h4 class="text-center">{Categoria}</h4>
+
+						
+						<ul class="listviGeral">
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 1
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 2
+											</span>
+											<span class="valorItem backred pull-right"> 
+												R$ -200,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Conta 3
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+						</ul>
+
+						
+					</div>
+
+				</div>
+
+			</div>
+
+		</div><!-- Fim card contas Poupança-->
+
+<!--Inicio card contas AOC -->
+		<div class="row">
+			<div class="col-md-10 col-lg-8 col-lg-offset-2 col-md-offset-1">
+				<div class="box">
+					<div class="box-icon">
+						<span class="fa fa-3x fa-bank"></span>
+
+					</div>
+					<div class="info">
+						<h4 class="text-center">{Categoria}</h4>
+
+						
+						<ul class="listviGeral">
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Despesa 1
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Despesa 2
+											</span>
+											<span class="valorItem backred pull-right"> 
+												R$ -200,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+							<li>
+								<a href="#" title="">
+										<div class="listitemBox">
+
+											<span class="listviItem">
+											<i class="fa fa-dollar green pull-left "></i>
+												Despesa 3
+
+												<i style="font-weight:100;font-size:15;">( Pendente )</i>
+											</span>
+											<span class="valorItem backgreen pull-right"> 
+												R$ 100,00
+											</span>
+											
+										</div>
+									</a>
+							</li>
+						</ul>
+
+						
+					</div>
+
+				</div>
+
+			</div>
+
+		</div><!-- Fim card contas AOC-->
